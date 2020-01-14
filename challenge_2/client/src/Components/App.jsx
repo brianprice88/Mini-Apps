babac94bc195801faf03b23b5170fd9528d448fe
Brian Price <brianprice88@gmail.com>
2020-01-14 11:22:26 -0800

    "Server connected"

---
+++ challenge_2/client/src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+}
+
+
+
+
+
+
+export default App;
