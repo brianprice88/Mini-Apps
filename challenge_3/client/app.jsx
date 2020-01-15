41ebfb0c5f725cdd46a7b79f065797c020f18028
Brian Price <brianprice88@gmail.com>
2020-01-15 12:27:34 -0800

    "Completed setup after trying to use Babel all morning and finally going to Webpack.  Can actually start on the assignment now..."

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,20 @@
+import React from "React";
+import ReactDom from "React-Dom";
 
+class App extends React.Component {
+  constructor(props) {
+      super(props)
+  }
 
+  render() {
+      return (
+          <div>
+              <h1>hi</h1>
+          </div>
+      )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
+
+export default App;
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
+const express = require('express')
+const app = express();
 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({extended: true}));
+app.use(cors());
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/api', (req, res) => {res.send('hi')})
+
+
+app.listen(3000, () => {console.log('server listening on port 3000!')})
 
