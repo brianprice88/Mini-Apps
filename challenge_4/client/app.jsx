bf8d436bdc075dd01ef3152a573fe42981261fe4
Brian Price <brianprice88@gmail.com>
2020-01-16 09:40:15 -0800

    "Set up server and client for Connect Four.  Beginning to create board"

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,26 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          turn: true
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            <h2 id = 'nowUp'>Now up: <span style={{color: 'purple'}}>{this.state.turn ? 'black' : 'red'}</span></h2>
+              {/* <Board /> */}
+            </div>
+        )
+    }
+
+}
 
 
+
+
+ReactDom.render(<App />, document.getElementById('app'))
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.static('public'));
+
+
+app.listen(3000, () => (console.log('express listening on port 3000!')))
