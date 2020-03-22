c1c8e1f9fd4af6de49b19dc3e479d5b52591c16d
Brian Price <brianprice88@gmail.com>
2020-03-22 19:58:50 -0400

    "Update file structure"

--- challenge_4/client/Headers.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'React'
-//player who's going can click a header column to drop a piece there, which changes to other player's turn
-//NEED TO ADD CONDITION THAT THIS BUTTON ONLY EXISTS IF COLUMN ISN'T FULL
-var Headers = (props) => (
-    <th><button onClick = {(event) => (props.changeTurn(event))}>{props.name} Drop piece here</button> </th>
-)
-
-export default Headers
--- challenge_4/client/app.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Board from './board.jsx';
-
-ReactDom.render(<Board />, document.getElementById('app'))
-
